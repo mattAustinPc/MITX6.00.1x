9682b9af7517600bc047e5d44d934f9228f923f4
Matt Austin <matthewaustin@utilita.co.uk>
2025-02-05 22:00:08 +0000

    mitx60001x: add string exercises file

---
+++ strings_exercises.py
@@ -0,0 +1,4 @@
+test_string = "Please hack chop and slice me up into characters for your learning."
+test_slice_copy = test_string[0:0]
+print(f"This is the original variable: {test_string}")
+print(test_slice_copy)
